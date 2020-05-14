bc0b8227b44a7a1d3f97b53cb2e9e98186691d54
JacksonDev <jzambranogarzon@gmail.com>
2020-05-14 11:50:19 -0500

    Build a production 1/2

--- src/services/AuthToken.js
+++ src/services/AuthToken.js
@@ -1,8 +1,8 @@
 import { generateRandomString } from "../services/generateRandomString";
 
 export const authorize = () => {
-  let client_id = "7cd459dfc6a94248aa12f166d777a0e2"; // Your client id
-  let redirect_uri = "https://clon-spotify-app.herokuapp.com/webplayer/"; // Your redirect uri
+  const client_id = "7cd459dfc6a94248aa12f166d777a0e2"; // Your client id
+  const redirect_uri = "https://clon-spotify-app.herokuapp.com/webplayer/"; // Your redirect uri
 
   let state = generateRandomString(16);
 
